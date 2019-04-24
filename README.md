0e8d7a1c411aff5def83a1f5d90fcbf77dcbb494
ichrispa <Chris_Paul.Iatrou@tu-dresden.de>
2019-04-24 10:23:29 +0200

    Copied current design

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# PLT_MRT_Ampelschaltung
-Schaltpläne und Platinenlayouts für die Ampelschaltung. Dient als persistentere Alternative zum Breadboard.
+# The PLT/MRT Trafficlight
+
+This repository contains the schematics and pcb board masks for the traffic light that is used as a lecture example in the Microcomputingtechnology 1 and 2 courses of the Chair for *Process Control and Process Systems Engineering Working Group*. While students are provided with instructions on how to create a simple example circuit using bread boards, this PCB gives them a more durable, multi-purpose I/O circuit.
+
+## Schematics
+
+Schematics a are included in the `schem` folder and can be edited using [GNU Schem](http://wiki.geda-project.org/geda:gaf#utilities_for_schematics), part of the GEDA project.
+
+You can find printable PDF Versions of the schematic in `doc` (Postscript and PDF).
+
+## Board Layout
+
+An example board layout is included in `pcb` and can be edited using [GNU PCB](http://pcb.geda-project.org), part of the GEDA project.
+
+You can find printable PDF Versions of the board and drill masks in `doc` (Postscript and PDF).
+
+
+[![Open Source Hardware](https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Open-source-hardware-logo.svg/200px-Open-source-hardware-logo.svg.png)](https://solderpad.org/licenses/SHL-2.0/)
---
+++ pcb/ampel_3licht.cmd
@@ -0,0 +1,88 @@
+# Pin name action command file
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element S1
+ChangePinName(S1, 2, 2)
+ChangePinName(S1, 1, 1)
+
+# Start of element CONN1
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 2, 2)
+ChangePinName(CONN1, 1, 1)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element Q3
+ChangePinName(Q3, 1, E)
+ChangePinName(Q3, 3, C)
+ChangePinName(Q3, 2, B)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element D6
+ChangePinName(D6, 2, 2)
+ChangePinName(D6, 1, 1)
+
+# Start of element D5
+ChangePinName(D5, 2, 2)
+ChangePinName(D5, 1, 1)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element Q2
+ChangePinName(Q2, 1, E)
+ChangePinName(Q2, 3, C)
+ChangePinName(Q2, 2, B)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element D4
+ChangePinName(D4, 2, 2)
+ChangePinName(D4, 1, 1)
+
+# Start of element D3
+ChangePinName(D3, 2, 2)
+ChangePinName(D3, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element Q1
+ChangePinName(Q1, 1, E)
+ChangePinName(Q1, 3, C)
+ChangePinName(Q1, 2, B)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
